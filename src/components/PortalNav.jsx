15ab38a47c4d86c173361cd8aaa122784150dcc4
ahmadfaraj4000-gif <ahmad.faraj4000@gmail.com>
2026-06-26 02:15:57 -0400

    Add files via upload

--- src/components/PortalNav.jsx
+++ src/components/PortalNav.jsx
@@ -2,15 +2,9 @@ import logoUrl from "../assets/logo.png";
 
 export function PortalNav() {
   return (
-    <nav className="nav">
+    <nav className="nav portalNav">
       <div className="wrap navin">
-        <a className="brand" href="../index.html"><img src={logoUrl} alt="The Anchor Collective logo" /><span>THE ANCHOR COLLECTIVE</span></a>
-        <div className="links">
-          <a href="../events.html">Events</a>
-          <a href="../family-resources.html">Family Resources</a>
-          <a href="../podcasts.html">Podcasts</a>
-          <a href="../donations.html">Donate</a>
-        </div>
+        <div className="brand"><img src={logoUrl} alt="The Anchor Collective logo" /><span>THE ANCHOR COLLECTIVE</span></div>
       </div>
     </nav>
   );
